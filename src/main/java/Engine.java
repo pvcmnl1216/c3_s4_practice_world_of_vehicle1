5d836430055cafe118fc9711ac226c33b457f14f
Philip Meshach <philipmeshach.pm@gmail.com>
2022-08-29 12:46:02 +0530

    The no args constructor and Parameteriazed constructors are created

--- src/main/java/Engine.java
+++ src/main/java/Engine.java
@@ -11,5 +11,20 @@ public class Engine {
     int maxRpm;
     String noOfCylinders;
 
+    public Engine() {
+    }
 
+    public Engine(int modelNumber, String displacement, String maxPower, int maxRpm, String noOfCylinders) {
+        this.modelNumber = modelNumber;
+        this.displacement = displacement;
+        this.maxPower = maxPower;
+        this.maxRpm = maxRpm;
+        this.noOfCylinders = noOfCylinders;
+    }
+
+    class ElectricEngines {
+
+        String Voltage;
+
+    }
 }
