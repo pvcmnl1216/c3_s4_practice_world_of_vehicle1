1babec7a2fa81780b342bc4437ffb90027f46299
Philip Meshach <philipmeshach.pm@gmail.com>
2022-08-29 12:42:41 +0530

    the engine class is created

---
+++ src/main/java/Engine.java
@@ -0,0 +1,15 @@
+/*
+ * Author Name: Philip Meshach
+ * Date: 29-08-2022
+ * Praise The Lord
+ */
+
+public class Engine {
+    int modelNumber;
+    String displacement;
+    String maxPower;
+    int maxRpm;
+    String noOfCylinders;
+
+
+}
