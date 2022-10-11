e6586d110c0783a841358f2e1659b4ba4f2b2773
Philip Meshach <Philipmeshach.pm@gmail.com>
2022-10-11 12:35:12 +0530

    created the EvEngine class

---
+++ src/main/java/EvEngine.java
@@ -0,0 +1,11 @@
+/*
+ * Author Name: Philip Meshach
+ * Date: 11-10-2022
+ * Praise The Lord
+ */
+
+public class EvEngine extends Engine {
+    private double voltage;
+    private String maxPower;
+    private int maxRpm;
+}
