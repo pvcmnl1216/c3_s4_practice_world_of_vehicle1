6c446e9d1471be18722cd796e692108b4aafbc39
Philip Meshach <Philipmeshach.pm@gmail.com>
2022-10-11 12:33:43 +0530

    created the constructors and getter setter to string methods in petrol engine class

--- src/main/java/PetrolEngine.java
+++ src/main/java/PetrolEngine.java
@@ -8,4 +8,53 @@ public class PetrolEngine extends Engine {
     private String maxPower;
     private int maxRpm;
     private String noOfCylinders;
+
+    public PetrolEngine() {
+    }
+
+    public PetrolEngine(int modelNumber, String displacement, String maxPower, int maxRpm, String noOfCylinders, String maxPower1, int maxRpm1, String noOfCylinders1) {
+        super(modelNumber, displacement, maxPower, maxRpm, noOfCylinders);
+        this.maxPower = maxPower1;
+        this.maxRpm = maxRpm1;
+        this.noOfCylinders = noOfCylinders1;
+    }
+
+    @Override
+    public String getMaxPower() {
+        return maxPower;
+    }
+
+    @Override
+    public void setMaxPower(String maxPower) {
+        this.maxPower = maxPower;
+    }
+
+    @Override
+    public int getMaxRpm() {
+        return maxRpm;
+    }
+
+    @Override
+    public void setMaxRpm(int maxRpm) {
+        this.maxRpm = maxRpm;
+    }
+
+    @Override
+    public String getNoOfCylinders() {
+        return noOfCylinders;
+    }
+
+    @Override
+    public void setNoOfCylinders(String noOfCylinders) {
+        this.noOfCylinders = noOfCylinders;
+    }
+
+    @Override
+    public String toString() {
+        return "PetrolEngine{" +
+                "maxPower='" + maxPower + '\'' +
+                ", maxRpm=" + maxRpm +
+                ", noOfCylinders='" + noOfCylinders + '\'' +
+                '}';
+    }
 }
